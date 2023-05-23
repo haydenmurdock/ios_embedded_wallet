366a2ced914c66dfa26d744b114775eb80d35e13
haydenmurdock <haydenmurdock18@gmail.com>
2023-05-23 16:04:52 -0600

    Initial Commit

---
+++ Embedded Wallet iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Embedded Wallet iOS
+//
+//  Created by Hayden Murdock on 5/23/23.
+//
+
+import UIKit
+import AriesAgent
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
